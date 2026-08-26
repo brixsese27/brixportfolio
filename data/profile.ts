3fa398b2a589c89ec0355affa16a1b19a593071d
Brix Sese <brixsese.dev@gmail.com>
2026-08-26 20:26:21 +0800

    linked and resume

--- data/profile.ts
+++ data/profile.ts
@@ -12,7 +12,7 @@ export const profileData: Profile = {
   email: "sesebrixligon@gmail.com",
   phone: "+639854026038",
   github: "https://github.com/brixsese27",
-  linkedin: "https://linkedin.com/in/brixsese",
+  linkedin: "https://www.linkedin.com/in/brix-sese-196607406/",
   resumeUrl: "/resume.pdf",
   bioParagraphs: [
     "A reliable and hardworking Bachelor of Science in Information Technology graduate with proven experience in office IT support, computer troubleshooting, and hardware/software maintenance. I am a fast learner, well-mannered, and dedicated to supporting organizational teams and end-users with high-quality technical service.",
